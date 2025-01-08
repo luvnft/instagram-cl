5f2e022423a438eba62dfc6c50a7984ed83af899
ang3lfco <ang3lfco@outlook.com>
2025-01-08 04:03:31 -0700

    authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
-export default function Home() {
+import { auth, signIn, signOut } from "@/auth";
+
+export default async function Home() {
+  const session = await auth();
   return (
-    <div className="bg-ig-red">test</div>
+    <div className="">
+      test<br/>
+      {session && (
+        <form action={async () => {
+          'use server';
+          await signOut();
+        }}>
+          <button
+            className="border px-4 py-2 bg-ig-red text-white rounded-lg"
+            type="submit">Logout
+          </button>
+        </form>
+      )}
+      {!session && (
+        <form action={async () => {
+          'use server';
+          await signIn('google');
+        }}>
+          <button
+            className="border px-4 py-2 bg-ig-red text-white rounded-lg"
+            type="submit">Login with google
+            
+          </button>
+        </form>
+      )}
+    </div>
   );
 }
