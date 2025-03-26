6c3419ca38397eeaa7e1f6089d6117031c9f5bff
ang3lfco <ang3lfco@outlook.com>
2025-03-26 00:09:48 -0700

    mobile navigation extras

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -33,23 +33,33 @@ export default function RootLayout({
       >
         <Theme>
           {children}
-          <div className="fixed bottom-0 bg-white px-4 py-2 left-0 right-0">
-            <div className="max-w-lg mx-auto flex justify-between *:size-12 *:flex *:items-center *:justify-center">
-              <Link href="/">
-                <Home/>
-              </Link>
-              <Link href="/search">
-                <Search/>
-              </Link>
-              <Link href="/create" className="bg-gradient-to-tr from-ig-orange to-ig-red text-white rounded-full relative -top-6">
-                <Camera/>
-              </Link>
-              <Link href="/browse">
-                <LayoutGrid/>
-              </Link>
-              <Link href="/profile">
-                <User/>
-              </Link>
+          <div className="fixed bottom-0 left-0 right-0">
+            <div className="flex *:flex *:items-center">
+              <div className="pl-2 bg-white rounded-t-xl w-full relative z-10 *:size-12 *:flex *:items-center *:justify-center justify-around">
+                <Link href="/" className="">
+                  <Home/>
+                </Link>
+                <Link href="/search" className="">
+                  <Search/>
+                </Link>
+              </div>
+              <div className="size-14 relative -top-4 justify-center w-[125px]">
+                <div className="absolute bg-blue-500 bg-clip-text border-white border-t-transparent border-l-transparent border-[50px] rounded-full rotate-45">
+                  <div className="border-4 size-15 border-transparent">
+                    <Link href="/create" className="-rotate-45 bg-gradient-to-tr from-ig-orange to-ig-red size-12 flex items-center justify-center text-white rounded-full ">
+                      <Camera/>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="pr-2 bg-white rounded-t-xl w-full relative z-10 *:size-12 *:flex *:items-center *:justify-center justify-around">
+                <Link href="/browse" className="">
+                  <LayoutGrid/>
+                </Link>
+                <Link href="/profile" className="text-ig-red ">
+                  <User/>
+                </Link>
+              </div>
             </div>
           </div>
         </Theme>
